18608dc06edc89819f26f80813339f6b52482e5b
Roberto Ortega <berto.ort@gmail.com>
2015-07-12 18:10:18 -0600

    score board and login

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
   $('.your').on('click', function (e) {
     $('.show_all').css('display', 'none');
+    $('.score_board').css('display', 'none')
     $('.show_your').css('display', 'inline-block');
     e.preventDefault();
   });
   $('.all').on('click', function (e) {
     $('.show_your').css('display', 'none')
+    $('.score_board').css('display', 'none')
     $('.show_all').css('display', 'inline-block')
   });
+  $('.score').on('click', function (e) {
+    $('.show_your').css('display', 'none')
+    $('.show_all').css('display', 'none')
+    $('.score_board').css('display', 'inline-block')
+  });
+  $('.clear').on('focus', function (e) {
+    this.blur();
+  })
 });
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,33 @@ var router = express.Router();
 var database = require('./../connection');
 var answers = database.get('trivia');
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login to Trivia Answers'});
+});
+
+router.post('/login', function(req, res, next) {
+  res.cookie('team', req.body.team);
+  res.redirect('/');
+});
+
+router.use(function (req, res, next) {
+  if (req.cookies.team === undefined) {
+    res.redirect('/login')
+  }
+  else {
+    next();
+  }
+})
+
 router.get('/', function(req, res, next) {
   answers.find({}, function (err, data) {
     res.render('index', { title: 'Trivia Answers', answers: data, team: req.cookies.team });
   })
+});
+
+router.post('/logout', function(req, res, next) {
+ res.clearCookie('team');
+ res.redirect('/');
 });
 
 router.post('/delete', function(req, res, next) {
@@ -14,11 +37,35 @@ router.post('/delete', function(req, res, next) {
   res.redirect('/');
 });
 
+router.post('/clear', function(req, res, next) {
+  answers.update({}, { $set: {answer: ""}}, {multi: true});
+  res.redirect('/');
+});
+
+router.post('/score', function(req, res, next) {
+  answers.find({}, function (err, data) {
+    for (var i = 0; i < data.length; i++) {
+      var teamNumber = i+100;
+      var team = req.body[teamNumber];
+      answers.findOne({team: team}, function (err, teamData) {
+        console.log(i);
+        console.log(teamData,req.body[i], teamData.score, teamData.team, 'second');
+        var point = teamData.score;
+        if (teamData.score === undefined) {
+          point = 0
+        }
+        var points = point + req.body[i];
+        answers.update({team: team}, {$set: {score: 0}})
+      })
+    }
+  })
+  // answers.update({}, { $set: {answer: ""}}, {multi: true});
+  res.redirect('/');
+});
+
 router.post('/answer', function(req, res, next) {
-  res.cookie('team', req.body.team);
-  // answers.update({team: req.body.team}, {team: req.body.team, answer: req.body.answer}, {upsert: true});
-  answers.remove({team: req.body.team});
-  answers.insert({team: req.body.team, answer: req.body.answer});
+  answers.remove({team: req.cookies.team});
+  answers.insert({team: req.cookies.team, answer: req.body.answer});
   res.redirect('/');
 });
 
